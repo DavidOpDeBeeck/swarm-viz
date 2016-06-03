a8653233a2531fe2420d7331eeaea2bb60dd7196
David Op de Beeck <davidopdebeeck@hotmail.com>
2016-06-03 10:31:39 +0200

    AngularJS: Host view, Cluster information, Container search and Settings
    + MIT license
    + AngularJS front end
    
    Added ES6 syntax to viz.js and restructured the file.

---
+++ static/app.js
@@ -0,0 +1,270 @@
+(function() {
+  "use strict"
+
+  var app = angular.module("swarm-ui", [
+    "btford.socket-io",
+    "angular-nicescroll"
+  ]);
+
+  app.run(function (LocalStorage) {
+    if (!LocalStorage.exists('displayUptime'))
+      LocalStorage.set('displayUptime', 'true');
+    if (!LocalStorage.exists('displayNetworks'))
+      LocalStorage.set('displayNetworks', 'true');
+    if (!LocalStorage.exists('displayEmptyHosts'))
+      LocalStorage.set('displayEmptyHosts', 'true');
+    if (!LocalStorage.exists('displayExitedContainers'))
+      LocalStorage.set('displayExitedContainers', 'true');
+  });
+
+  app.factory('socket', function (socketFactory) {
+    var socket = socketFactory();
+    socket.forward('containers');
+    return socket;
+  });
+
+  app.service('LocalStorage', function ($rootScope, $window) {
+
+    var localStorage = {
+      set     : set,
+      get     : get,
+      getBool : getBool,
+      exists  : exists
+    }
+
+    return localStorage;
+
+    //////////////////
+
+    function set( key , value ) {
+      $rootScope.$broadcast('LocalStorage.notification.set',{ key: key, value: value });
+      $window.localStorage.setItem(key, value)
+    }
+
+    function get( key ) {
+      return $window.localStorage.getItem(key);
+    }
+
+    function getBool( key ) {
+      return get(key) === 'true';
+    }
+
+    function exists( key ) {
+      return get(key) !== undefined;
+    }
+
+  });
+
+  app.service('DataService', function ($rootScope) {
+
+    var hosts = {};
+
+    $rootScope.$on('socket:containers', function (ev, data) {
+      var temp = data.hosts;
+      for ( var host in temp ) {
+        hosts[host] = {
+          name : host,
+          containers : temp[host]
+        }
+      }
+      $rootScope.$broadcast('DataService.notification.refresh');
+    });
+
+    //////////////////
+
+    var dataService = {
+      getHosts : getHosts,
+      getContainers : getContainers
+    }
+
+    return dataService;
+
+    //////////////////
+
+    function getHosts() {
+      return hosts;
+    }
+
+    function getContainers() {
+      var containers = [];
+      Object.values(hosts).forEach(function (h) { containers = containers.concat(h.containers); });
+      return containers;
+    }
+
+  });
+
+  app.controller('overviewCtrl', function (socket, $scope, LocalStorage, DataService) {
+
+    var vm = this;
+
+    //////////////////
+
+    vm.displayUptime;
+    vm.displayNetworks;
+    vm.displayEmptyHosts;
+    vm.displayExitedContainers;
+    vm.diplaySwarmContainers;
+
+    vm.hosts = DataService.getHosts();
+
+    vm.showHost = showHost;
+    vm.showContainer = showContainer;
+
+    activate();
+
+    //////////////////
+
+    function activate () {
+      vm.displayUptime = LocalStorage.getBool('displayUptime');
+      vm.displayNetworks = LocalStorage.getBool('displayNetworks');
+      vm.displayEmptyHosts = LocalStorage.getBool('displayEmptyHosts');
+      vm.displayExitedContainers = LocalStorage.getBool('displayExitedContainers');
+      vm.displaySwarmContainers = LocalStorage.getBool('displaySwarmContainers');
+    }
+
+    function showHost(host) {
+      return vm.displayEmptyHosts ? true : host.containers.length > 0;
+    }
+
+    function showContainer(container) {
+      return vm.displayExitedContainers ? true : container.state !== 'exited';
+    }
+
+    $scope.$on('LocalStorage.notification.set', function(event, parameters) {
+       switch( parameters.key ) {
+         case "displayUptime":
+            return vm.displayUptime = parameters.value;
+         case "displayNetworks":
+            return vm.displayNetworks = parameters.value;
+         case "diplaySwarmContainers":
+            return vm.diplaySwarmContainers = parameters.value;
+         case "displayEmptyHosts":
+            return vm.displayEmptyHosts = parameters.value;
+         case "displayExitedContainers":
+            return vm.displayExitedContainers = parameters.value;
+       }
+    });
+  });
+
+  app.controller('informationCtrl', function ($scope, DataService) {
+
+    var vm = this;
+
+    //////////////////
+
+    vm.totalHosts;
+    vm.totalContainers;
+    vm.exitedContainers;
+    vm.runningContainers;
+
+    activate();
+
+    //////////////////
+
+    function activate() {
+      var hosts = DataService.getHosts();
+      var containers = DataService.getContainers();
+
+      vm.totalHosts         = Object.keys(hosts).length;
+      vm.totalContainers    = containers.length;
+      vm.exitedContainers   = containers.filter(function(c) { return c.state === 'exited'; }).length;
+      vm.runningContainers  = containers.filter(function(c) { return c.state === 'running'; }).length;
+    }
+
+    $scope.$on('DataService.notification.refresh', function (ev, data) {
+      activate();
+    });
+
+  });
+
+  app.controller('searchCtrl', function ($scope, LocalStorage, DataService) {
+
+    var vm = this;
+    var containers = [];
+
+    //////////////////
+
+    vm.query;
+    vm.results;
+
+    vm.search = search;
+
+    //////////////////
+
+    function activate() {
+      containers = DataService.getContainers();
+    }
+
+    function search() {
+      vm.results = containers.filter(function (c) {
+        return (vm.query && (c.name.indexOf(vm.query) > -1 || c.image.indexOf(vm.query) > -1))
+      });
+    }
+
+    $scope.$on('DataService.notification.refresh', function (ev, data) {
+      activate();
+    });
+
+  });
+
+  app.controller('settingsCtrl', function ($scope, LocalStorage) {
+
+    var vm = this;
+
+    //////////////////
+
+    vm.displayUptime;
+    vm.displayNetworks;
+    vm.displayEmptyHosts;
+    vm.displayExitedContainers;
+    vm.diplaySwarmContainers;
+
+    vm.toggleUptime = toggleUptime;
+    vm.toggleNetworks = toggleNetworks;
+    vm.toggleEmptyHosts = toggleEmptyHosts;
+    vm.toggleExitedContainers = toggleExitedContainers;
+    vm.toggleSwarmContainers = toggleSwarmContainers;
+
+    activate();
+
+    //////////////////
+
+    function activate() {
+      vm.displayUptime = LocalStorage.getBool('displayUptime');
+      vm.displayNetworks = LocalStorage.getBool('displayNetworks');
+      vm.displayEmptyHosts = LocalStorage.getBool('displayEmptyHosts');
+      vm.displayExitedContainers = LocalStorage.getBool('displayExitedContainers');
+      vm.displaySwarmContainers = LocalStorage.getBool('displaySwarmContainers');
+    }
+
+    function toggleUptime() {
+      LocalStorage.set('displayUptime', Boolean(vm.displayUptime));
+    }
+
+    function toggleNetworks() {
+      LocalStorage.set('displayNetworks', Boolean(vm.displayNetworks));
+    }
+
+    function toggleEmptyHosts() {
+      LocalStorage.set('displayEmptyHosts', Boolean(vm.displayEmptyHosts));
+    }
+
+    function toggleExitedContainers() {
+      LocalStorage.set('displayExitedContainers', Boolean(vm.displayExitedContainers));
+    }
+
+    function toggleSwarmContainers() {
+      LocalStorage.set('displaySwarmContainers', Boolean(vm.displaySwarmContainers));
+    }
+
+  });
+
+})();
+
+Object.values = function(object) {
+  var values = [];
+  for(var property in object) {
+    values.push(object[property]);
+  }
+  return values;
+}
--- viz.js
+++ viz.js
@@ -1,122 +1,108 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var port = 3000;
-var refreshTime = 2000;
+//---------------------------
+// Modules
+//---------------------------
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/static/index.html');
-});
+"use strict"
 
-http.listen(port, function(){
-  console.log('listening on *:3000');
-});
+const express = require('express');
+const app = express();
+const http = require( 'http' ).Server( app );
+const io = require( 'socket.io' )( http );
+const docker = require( 'dockerode' );
+const fs = require( 'fs' );
 
-var Docker = require('dockerode');
-var fs = require('fs');
+//---------------------------
+// Globals
+//---------------------------
 
+const port = 3000;
+const refreshTime = 2000;
 var dockerHost = process.env.SWARM_HOST.match(/tcp:\/\/(.*):.*$/)[1];
 var dockerPort = process.env.SWARM_HOST.match(/tcp:\/\/.*:(.*)$/)[1];
 var dockerCertPath = process.env.DOCKER_CERT_PATH;
-console.log(dockerHost);
-console.log(dockerPort);
 
-var docker = new Docker({
-  protocol: 'https', //you can enforce a protocol
-  host: dockerHost,
-  port: dockerPort,
-  ca: process.env.SWARM_CA || fs.readFileSync(dockerCertPath + '/ca.pem'),
-  cert: process.env.SWARM_CERT || fs.readFileSync(dockerCertPath + '/cert.pem'),
-  key: process.env.SWARM_KEY || fs.readFileSync(dockerCertPath + '/key.pem')
+const dConnection = new docker({
+    protocol: 'https',
+    host: dHost,
+    port: dPort,
+    ca: process.env.SWARM_CA || fs.readFileSync(dCertPath + '/ca.pem'),
+    cert: process.env.SWARM_CERT || fs.readFileSync(dCertPath + '/server.pem'),
+    key: process.env.SWARM_KEY || fs.readFileSync(dCertPath + '/server-key.pem')
 });
 
-function listContainers(callback) {
-  var res = {hosts: {}};
-  docker.listContainers(function (err, containers) {
-    if (containers) {
-      containers.forEach(function (containerInfo) {
-        var names = containerInfo.Names;
+//---------------------------
+// Routes
+//---------------------------
 
-        if (!names.length) {
-          return;
-        }
+app.get( '/', ( req, res ) => {
+    res.sendFile( __dirname + '/static/index.html' );
+});
 
-        var tokens = names[0].split('/');
+app.use(express.static('static'));
 
-        if (tokens.length < 2) {
-          return;
-        }
+http.listen( port, () => {
+    console.log( 'listening on *:3000' );
+});
 
-        var host = tokens[1];
-        var name = tokens[2];
+//---------------------------
+// Docker container fetch
+//---------------------------
 
-        var data = {
-          id: containerInfo.Id,
-          image: containerInfo.Image
-        };
+function listNetworks( callback ) {
+  dConnection.listNetworks({
+    all : 1
+  }, ( err, networks ) => {
+    networks.forEach( nInfo => {
 
-        if (name.indexOf('swarm-agent') >= 0) {
-          if (!res.hosts[host]) {
-            res.hosts[host] = [];
-          }
-          return;
-        }
-
-        if (host in res.hosts) {
-          res.hosts[host].push(data);
-        }
-        else {
-          res.hosts[host] = [data];
-        }
-      });
-      callback(res);
-    }
-    setTimeout(function () {
-      listContainers(callback);
-    }, 1000);
+    });
   });
 }
 
-listContainers(function (containers) {
-  io.emit('containers', containers);
-});
+function listContainers( callback ) {
+    let res = {
+        hosts: {}
+    };
+    dConnection.listContainers({
+      all: 1
+    }, ( err, containers ) => {
+        if ( containers ) {
+            containers.forEach( cInfo => {
+                let names = cInfo.Names;
 
-// IF ABOVE IS STILL SLOW: make use of below to trigger based on events
-/*
-docker.getEvents(function (error, stream) {
-  if (error) {
-    throw new Error('Could not listen to Docker events');
-  }
+                if ( !names.length ) return;
 
-  stream.setEncoding('utf8');
-  stream.on('data', function (json) {
-    var data = JSON.parse(json);
+                let tokens = names[ 0 ].split( '/' );
 
-    if (data.status === 'create') {
-      console.log('Container created.');
-      var container = {
-        id: data.id,
-        image: data.from.split(' ')[0]
-      };
-      var host = data.node.Name;
-      if (data.node.Name in res.hosts) {
-        res.hosts[host].push(container);
-      } else {
-        res.hosts[host] = [container];
-      }
-      io.emit('containers', res);
-    } else if (data.status === 'destroy' || data.status === 'kill' || data.status === 'stop') {
-      console.log('Container died.');
-      var host = data.node.Name;
-      console.log(host);
-      console.log(res.hosts);
-      if (res.hosts[host]) {
-        res.hosts[host] = res.hosts[host].filter(function (container) {
-          return container.id !== data.id;
-        });
-        io.emit('containers', res);
-      }
-    }
-  });
-});
-*/
+                if ( tokens.length < 2 ) return;
+
+                let host = tokens[ 1 ];
+                let name = tokens[ 2 ];
+
+                let data = {
+                    id: cInfo.Id,
+                    name: name,
+                    image: cInfo.Image.split(":")[0],
+                    state: cInfo.State,
+                    status: cInfo.Status,
+                    created: cInfo.Created,
+                    networks: Object.keys(cInfo.NetworkSettings.Networks)
+                };
+
+                if ( !res.hosts[ host ] )
+                    res.hosts[ host ] = [];
+
+                res.hosts[ host ].push( data );
+            });
+
+            callback( res );
+        }
+        setTimeout( function () {
+            listContainers( callback );
+            //listNetworks();
+        }, refreshTime );
+    } );
+}
+listNetworks();
+listContainers( function ( containers ) {
+    io.emit( 'containers', containers );
+} );
